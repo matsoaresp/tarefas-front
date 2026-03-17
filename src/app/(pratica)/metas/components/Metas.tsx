d3adf083c126818b211717d5e0b6d923e9e818d2
Mateus Prado <maateussp@gmail.com>
2026-03-17 11:34:37 -0300

    feat: adicionando metas

--- src/app/(pratica)/metas/components/Metas.tsx
+++ src/app/(pratica)/metas/components/Metas.tsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+'use client'
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
-    interface Metas {
+interface Metas {
 
-        id: number;
-        nome: string;
-        descricao: string;
-        horasAtuais?: number;
-        horasMetas?: number;
-    }
+    id: number;
+    nome: string;
+    descricao: string;
+    horasAtuais?: number;
+    horasMetas?: number;
+}
 
 export function Metas() {
 
@@ -16,14 +18,60 @@ export function Metas() {
     const [descricao, setDescricao] = useState("");
 
 
-    
+    const handleAddMeta = () => {
+
+
+        if(!nome.trim() || !descricao.trim()) {
+            toast.error("Preencha todos os campos")
+            return
+        }
+
+        const newMeta = [
+            ...metas,
+            { id: Date.now(), nome: nome.trim(), descricao: descricao.trim() }
+        ]
+
+        toast.success("Meta adicionada com sucesso");
+        setMetas(newMeta)
+        localStorage.setItem("metas", JSON.stringify(newMeta))
+        setNome("")
+        setDescricao("")
+        
+    }
+
+    useEffect(() => {
+        const data = localStorage.getItem("metas");
+        if (data){
+            setMetas(JSON.parse(data))
+        }
+    }, [])
 
     return (
         <div>
             <input type="
-            " placeholder="Nome"/>
+            " placeholder="Nome"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)} />
             <input type="text"
-            placeholder="Descricao" />
+                placeholder="Descricao"
+                value={descricao}
+                onChange={(e) => setDescricao(e.target.value)} />
+
+            <button
+                onClick={handleAddMeta}
+            >
+                Adicionar Meta
+            </button>
+            <ul className="flex">
+                {metas.map((metas) =>(
+
+                    <li className="gap-4" key={metas.id}>
+                        Nome: {metas.nome}
+                        Descrição: {metas.descricao}
+                    </li>
+                    
+                ))}
+            </ul>
         </div>
     )
 }
